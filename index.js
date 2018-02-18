e40c5aa488abb35368e2199ae0775c5dcd249948
Ray <ray@protozo.net>
2018-02-18 11:57:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ var recipes = {};
 
 function updateObjectWithKeyAndValue(object, key, value)
 {
-  var newRecipe = object.assign({}, object);
+  var newRecipe = object.assign({}, object, key: value);
+  
   return object;
   
 }
